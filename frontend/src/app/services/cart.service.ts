9ecf1f3297730862ec5f6531bbdbf769928646ae
Akonomy <akonomy.official@gmail.com>
2023-08-07 18:11:57 +0300

    finished cart page

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -8,25 +8,32 @@ import { CartItem } from '../shared/models/CartItem';
   providedIn: 'root'
 })
 export class CartService {
+  
   private cart: Cart = this.getCartFromLocalStorage();
   private cartSubject: BehaviorSubject<Cart> = new BehaviorSubject(this.cart);
     constructor() { }
 
 
 addToCart(food:Food):void{
+  //console.log("Addto cart() is running.");
 
   let cartItem = this.cart.items
   .find(item => item.food.id === food.id);
   if (cartItem)
-    return;
+  cartItem.quantity++;
 
+  else{
   this.cart.items.push(new CartItem(food));
+  }
+
+  this.setCartToLocalStorage();
 
 }
 
 
 removeFromCart(foodId:string):void{
   this.cart.items = this.cart.items.filter(item => item.food.id != foodId);
+  this.setCartToLocalStorage();
 
 }
 
@@ -38,13 +45,16 @@ changeQuantity(foodId:string , quantity:number){
 
   cartItem.quantity=quantity;
   cartItem.price= quantity * cartItem.food.price
+  this.setCartToLocalStorage();
 }
 
 
 
 clearCart() {
+  //console.log("clearCart() is running.");
 
   this.cart =new Cart();
+  this.setCartToLocalStorage();
 }
 
 
@@ -59,21 +69,34 @@ return this.cartSubject.asObservable();
 //now for local storage
 
 
-private setCartToLocalStorage():void{
-  this.cart.totalPrice= this.cart.items.reduce((prevSum,currentItem) => prevSum+currentItem.price,0);
-  this.cart.totalCount= this.cart.items.reduce((prevSum,currentItem) => prevSum+currentItem.quantity,0);
-  const cartJson = JSON.stringify(this.cart);
-  localStorage.setItem("Cart",cartJson);   //name here "Cart"
-  this.cartSubject.next(this.cart);
+private setCartToLocalStorage(): void {
+
+  //console.log("setCartToLocalStorage() is running.");
+
+  this.cart.totalPrice = this.cart.items.reduce((prevSum, currentItem) => prevSum + currentItem.price, 0);
+  this.cart.totalCount = this.cart.items.reduce((prevSum, currentItem) => prevSum + currentItem.quantity, 0);
+
+  try {
+    const cartJson = JSON.stringify(this.cart);
+    localStorage.setItem('Cart', cartJson);
+    this.cartSubject.next(this.cart);
+  } catch (error) {
+    console.error('Error saving cart to localStorage:', error);
+  }
 }
 
+private getCartFromLocalStorage(): Cart {
 
-private getCartFromLocalStorage():Cart{
+  const cartJson = localStorage.getItem("Cart");
+  //console.log("getCartFromLocalStorage() is running. cartJson:", cartJson);
 
-  const cartJson = localStorage.getItem("Cart")  //same and here "Cart"
-  return cartJson?  JSON.parse(cartJson) :new Cart();
-
-
+  try {
+    const cartJson = localStorage.getItem('Cart');
+    return cartJson ? JSON.parse(cartJson) : new Cart();
+  } catch (error) {
+    console.error('Error retrieving cart from localStorage:', error);
+    return new Cart();
+  }
 }
 
 
