f0c11fd69404746b82bde21e9807662eba8fce64
Akonomy <akonomy.official@gmail.com>
2023-08-13 17:00:11 +0300

    Login functionality

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,6 +15,10 @@ import { TitleComponent } from './components/partials/title/title.component';
 import { NotFoundComponent } from './components/partials/not-found/not-found.component';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+
 
 
 @NgModule({
@@ -32,14 +36,23 @@ import { ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     NgbRatingModule,
     HttpClientModule,
     ReactiveFormsModule,
+    ToastrModule.forRoot({
+      timeOut:3000,
+      positionClass:'toast-bottom-right',
+      newestOnTop:false
+    }),
 
-    
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+    
--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -5,3 +5,4 @@ export const FOODS_TAGS_URL = BASE_URL + "api/foods/tags"; //poti pune si FOODS_
 export const FOODS_BY_SEARCH_URL = FOODS_URL + "/search/";
 export const FOODS_BY_TAG_URL = FOODS_URL + "/tag/";
 export const FOOD_BY_ID_URL = FOODS_URL + "/";
+export const USER_LOGIN_URL = BASE_URL + 'api/users/login';
