e4ad7802d2e7c29bc8112bf02db38409fc941935
Akonomy <akonomy.official@gmail.com>
2023-08-23 19:05:44 +0300

    Deploy app

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
+import path from 'path';
 import express from "express";
 import cors from "cors";
 import foodRouter from './routers/food.router';
 import userRouter from './routers/user.router';
-import { dbConnect } from './configs/database.config';
 import orderRouter from './routers/order.router';
+import { dbConnect } from './configs/database.config';
 dbConnect();
 
 const app = express();
@@ -20,7 +20,12 @@ app.use("/api/foods", foodRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
-const port = 5000;
+app.use(express.static('public'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'public', 'index.html'))
+})
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log("Website served on http://localhost:" + port);
 })
