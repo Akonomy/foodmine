420d2051c2f93ca1c7b3322e6a0fa1e469e19996
Akonomy <akonomy.official@gmail.com>
2023-08-27 18:55:18 +0300

    updated some css and improved for local test

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -20,12 +20,15 @@ app.use("/api/foods", foodRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
-app.use(express.static('public'));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'public', 'index.html'))
-})
+// app.use(express.static('public'));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname,'public', 'index.html'))
+// })
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
+
+const port = 5000;
+
 app.listen(port, () => {
     console.log("Website served on http://localhost:" + port);
 })
